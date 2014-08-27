6dbe46d64bdf95d2e2994d4409edd882b599bac1
Neil McGillivray <nmcgillivray@mac.com>
2014-08-27 10:58:02 +0200

    Plugin Name: spelling correction
    wordppress instead of worpress

--- bookshelf.php
+++ bookshelf.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: WorPress Goodreads Bookshelf
+Plugin Name: WordPress Goodreads Bookshelf
 Version: beta
 Plugin URI: http://thoughtcramps.com/wordpress-goodreads-bookshelf/
 Description: Display a custom list of books from your <a href="http://www.goodreads.com" target="_blank">Goodreads</a> bookshelves.  You can choose from any one of your bookshelves including your custom shelves on Goodreads.  Then you can stylize the display of the list however you want using your own HTML and CSS.
